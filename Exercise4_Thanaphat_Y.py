60edecc25645a9b6b1347fc3af7e552fe3086b24
skilltestz2423 <masterjaa01@gmail.com>
2021-04-14 17:12:10 +0700

    Exercise_4

---
+++ Exercise4_Thanaphat_Y.py
@@ -0,0 +1,16 @@
+'''
+รายชื่อวิชาดังนี้
+FE  = Foundation English
+GB  = General Business
+ICS = Introduction to Computer Systems
+CP  = Computer Programming
+'''
+FE  =  60.5
+GB  =  70
+ICS = 80.5
+CP  = 90
+print('-------Your Score------')
+print('Foundation English :', FE,'คะแนน')
+print('General Bussiness  :', GB,'คะแนน')
+print('Inrroduction to Computer System :', ICS, 'คะแนน')
+print('Computer Programming :', CP,'คะแนน')

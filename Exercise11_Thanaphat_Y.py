bd182e57874b896b682b8be9406b4c499f74e293
skilltestz2423 <masterjaa01@gmail.com>
2021-04-15 17:08:18 +0700

    version2

--- Exercise11_Thanaphat_Y.py
+++ Exercise11_Thanaphat_Y.py
@@ -1,11 +1,12 @@
 '''
-n = int(input('Test: '))
 for i in range(n):
     space = ' '*(n-i)
     print(space,"*"*int((i*2)-1))
 '''
-for x in range(n):
-    for y in range(x):
-        text+= "*"*((i*2)-1)
+n = int(input('Test: '))
+for x in range(n): #[0,1,2]
+    for y in range(x+1): #range([1,2,3])
+        text= ''
+        text+= "*"*((x*2)+1) #(y=[0],y=[0,1],y=[0,1,2])
     space =' '*(n-x)
     print(space,text)

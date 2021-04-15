855ac8d14548d3229bb48068ef77575c59a52e27
skilltestz2423 <masterjaa01@gmail.com>
2021-04-15 18:05:23 +0700

    Create Lecture50_Thanaphat_Y.py

---
+++ Lecture50_Thanaphat_Y.py
@@ -0,0 +1,14 @@
+def plusNumber(x,y):
+    print('ผลบวกคือ',x+y)
+def minusNumber(x,y):
+    print('ผลลบคือ',x-y)
+def mutipliedNumber(x,y):
+    print('ผลคูณคือ',x*y)
+def devidedNumber(x,y):
+    print('ผลหารคือ',x/y)
+
+plusNumber(1,2)
+minusNumber(5,3)
+mutipliedNumber(2,10)
+devidedNumber(25,5)
+
